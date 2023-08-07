551e468f1b02bebee242e28f7f21e7e79bad40d9
Garrick Ho <edugarrick@gmail.com>
2023-08-07 14:36:35 +0800

    .

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,15 @@ export REPO_NAME="sqlite-web"
 export DOCKERHUB_PATH="edugarrick"
 export TAG="2023.8.1"
 
+echo $0 $1
+
 echo "Build docker image ..."
-#docker buildx build --platform linux/amd64,linux/arm64/v8 -t $DOCKERHUB_PATH/$REPO_NAME:$TAG --push .
-docker buildx build --platform linux/arm64/v8 -t $DOCKERHUB_PATH/$REPO_NAME:$TAG --push .
-#docker buildx build --platform linux/amd64 -t $DOCKERHUB_PATH/$REPO_NAME:$TAG --push .
+platform=""
+if [ $1 = "all" ]; then
+   platform="--platform linux/amd64,linux/arm64/v8"
+fi
+
+echo "Platform: $platform"
+
+docker buildx build $platform -t $DOCKERHUB_PATH/$REPO_NAME:$TAG --push .
+docker buildx build -t $DOCKERHUB_PATH/$REPO_NAME:$TAG --push .
